615c9f87f0c2a963cea4533ff7325986d78b06fa
Daniel <danielselareid40@gmail.com>
2022-01-10 21:27:14 +1300

    added underscore as end-highlight character

--- client/scripts.js
+++ client/scripts.js
@@ -104,7 +104,7 @@ function renderInput(inputBox) { //highlight input, etc
 }
 
 const startHighlight = ['\\'];
-const endHighlight = ['&', ' ', '{','}','<','>','='];
+const endHighlight = ['&', ' ', '{','}','<','>','=', '_'];
 
 function colorInnerHTML(text) {
     //remove span tags from text
