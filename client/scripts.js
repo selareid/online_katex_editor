676099bb736e8a3d5ff27c145239b073a8fe0a04
Daniel <danielselareid40@gmail.com>
2022-01-17 13:24:30 +1300

    Render input box regardless of output render status

--- client/scripts.js
+++ client/scripts.js
@@ -62,11 +62,12 @@ function trySendNoteToServer(noteName, text, statusBox) {
 }
 
 function renderOI(text, inputBox, outputBox) {
+    renderInput(inputBox);
+
     var renderSuccess = tryRenderOutput(text, outputBox);
     if (renderSuccess) {
         inputBox.style.borderColor = '';
         inputBox.style.height = outputBox.clientHeight + 'px';
-        renderInput(inputBox);
     }
     else {
         inputBox.style.borderColor = 'red';
